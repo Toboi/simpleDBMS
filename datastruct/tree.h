594bf566b187f5316dc73cfc0b7a0ac8f7e6f9bf
Aeyon <tw3ttich@googlemail.com>
2012-04-16 22:28:25 +0200

    automated update: Mon, 16 Apr 2012 22:28:25

--- datastruct/tree.h
+++ datastruct/tree.h
@@ -2,7 +2,7 @@
 #define tree_h
 int tree_int_insert(int key, int val, int **rootNode);
 int tree_int_get(int key, int *rootNode, int* returnVal);
-int tree_int_remove(int key, int **rootNode);
+int tree_int_remove(int key, int **rootNode); // Wheat!!!!
 int tree_int_get_height(int *ptr);
 int tree_int_get_size(int *ptr);
 int tree_int_free_all(int **rootNode);
